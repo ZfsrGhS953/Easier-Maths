3fa92341e5caaf7d0737db1b218541d349e52aae
ZfsrGhS953 <j1355444@trbvm.com>
2017-09-02 22:36:57 +0300

    Create math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+function add(number1,number2){
+	var result=number1+number2;return result;
+}
+function Substract(number1, number2){
+	var result2= number1-number2;;
+	return result2
+};
+function multiple(number1, number2) {
+	var result3=number1*number2;
+	return result3;
+}
+function divide(number1,number2) {
+	var result4=number1/number2;return result4
+}
